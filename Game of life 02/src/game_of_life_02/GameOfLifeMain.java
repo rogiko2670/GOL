11015e213643e184254933fad01950afe22022b2
Your Name <you@example.com>
2023-10-09 22:41:03 +0200

    Ein neues Spiel erstellt und initialisiert

--- Game of life 02/src/game_of_life_02/GameOfLifeMain.java
+++ Game of life 02/src/game_of_life_02/GameOfLifeMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +16,16 @@ public class GameOfLifeMain extends Application{
 		 VBox root = new VBox(10);
 		 root.setPrefSize(Konst.WIDTH, Konst.HEIGHT + 60);
 		 Canvas canvas = new Canvas(Konst.WIDTH, Konst.HEIGHT);
+		 GraphicsContext gc = canvas.getGraphicsContext2D();
+		 
+		 // Die Anzahl der Zeilen und Spalten des Grids
+		 int rows = Math.floorDiv(Konst.WIDTH, Konst.CELL_SIZE);
+		 int columns = Math.floorDiv(Konst.HEIGHT, Konst.CELL_SIZE);
+		 
+		 // Eine neues Spiel
+		 Game game = new Game(rows, columns, gc);
+		 // das Spiel initialisieren
+		 game.gameInit();
 		 
 		 
 		 root.getChildren().add(canvas);
